c5a7383d48a6d74ae866620aecef4c3a7f2eada7
shaniscato <shaniscato@users.noreply.github.com>
2021-05-14 14:47:24 -0400

    play audio

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 
 class ViewController: UIViewController {
+
+    //call to func to play the audio
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     }
 
+    func soundFiles() {
+        let bundle = Bundle.main
 
+        guard let sound = bundle.path(forResource: "suga_boom_boom", ofType: "mp3")
+                else
+            {
+                return
+            }; do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+            print("song found")
+        } catch {
+            print("Song not found")
+        }
+    }
+
+    @IBAction func playSound(_ sender: UIButton) {
+        //fetch the song
+        soundFiles()
+        //play the sound
+        audioPlayer.play()
+    }
 
 }

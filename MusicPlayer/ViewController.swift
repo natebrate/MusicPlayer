2e06314861fc3bd98eccb7bac07a7f122479bba9
shaniscato <shaniscato@users.noreply.github.com>
2021-05-14 13:53:25 -0400

    Initial commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Software Engineering on 14/05/2021.
+//  Copyright © 2021 Software Engineering. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
